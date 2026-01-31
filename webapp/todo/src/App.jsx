1bb301adf8b4feb5a89c0666dbb4510c6c6a755e
HarshRaj2910 <harshroman617@gmail.com>
2026-01-31 16:54:52 +0530

    bb

--- webapp/todo/src/App.jsx
+++ webapp/todo/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import Header from './components/Header'
 
 function App() {
   return (
     <div>
-      <div className="header">
-        <h1>Header</h1>
-      </div>
+      <Header/>
     </div>
   )
 }
---
+++ webapp/todo/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+  return (
+    <div className='header'>
+      <h1>Our Website</h1>
+      <p>Our First Website</p>
+    </div>
+  )
+}
+
+export default Header
