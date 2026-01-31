5acbce3c30576f51e7807112fda6a0d765ce03c2
HarshRaj2910 <harshroman617@gmail.com>
2026-01-31 16:21:46 +0530

    v

--- webapp/todo/src/App.jsx
+++ webapp/todo/src/App.jsx
@@ -1,6 +1,18 @@
-function App(){
-  return (
-    <></>
+// function App(){
+//   return (
+//     <></>
+//   )
+// }
+// export default App;
+
+import React from "react";
+function App()
+{
+  return(
+  <>
+    <h1> First Website</h1>
+    <p> This is our first website</p>
+  </>  
   )
 }
 export default App;
