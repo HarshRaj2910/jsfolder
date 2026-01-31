ed3ce395c881fe762e354c7c15f1af79ed8ee982
HarshRaj2910 <harshroman617@gmail.com>
2026-01-31 11:44:32 +0530

    q

--- prog1.js
+++ prog1.js
@@ -1,1 +1,10 @@
 console.log("Hello World!!");
+
+var title="College" ;//  var -> global scope
+let data="AEC";  // let - limited scope
+const value=20;   // const - immutable , always give value
+
+console.log(title);
+console.log(data);
+console.log(value);
+
