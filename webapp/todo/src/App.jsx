5779ed8d8008fc122be60b85b591b342c703fa10
HarshRaj2910 <harshroman617@gmail.com>
2026-02-02 11:31:30 +0530

    e

--- webapp/todo/src/App.jsx
+++ webapp/todo/src/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Accounts from './pages/Accounts'
+import Profile from './pages/Profile'
 
 
 function App() {
@@ -16,7 +18,10 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}></Route>
           <Route path='/about' element={<About/>}></Route>
-          <Route path='/contact' element={<Contact/>}></Route>
+          <Route path='/contact' element={<Contact/>}>
+            <Route path='accounts' element={<Accounts/>}></Route>
+            <Route path='profile' element={<Profile/>}></Route>
+          </Route>
           <Route path='/services' ></Route>
         </Routes>
       </BrowserRouter>
--- webapp/todo/src/pages/Contact.jsx
+++ webapp/todo/src/pages/Contact.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Outlet,Link } from 'react-router-dom'
+
 
 function Contact() {
   return (
     <div>
-      <Link to=''>Profile</Link>
-      <Link to=''>Accounts</Link>
+      <Link to='profile'>Profile</Link>
+      <Link to='accounts'>Accounts</Link>
+      <Outlet/>
     </div>
   )
 }
