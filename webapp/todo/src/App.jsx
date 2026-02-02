76b59567bc9322e9c536702f4ecc026d48df8fbc
HarshRaj2910 <harshroman617@gmail.com>
2026-02-02 10:31:24 +0530

    a

--- webapp/todo/src/App.jsx
+++ webapp/todo/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Header from './components/Header'
+import Navigation from './components/Navigation'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <Header/>
+      <BrowserRouter>
+        <Navigation/>
+        
+      </BrowserRouter>
     </div>
   )
 }
--- webapp/todo/src/components/Navigation.jsx
+++ webapp/todo/src/components/Navigation.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 function Navigation() {
   return (
-    <div>
+    <div className='navbar'>
+    <Link to='' className='nav'>Home</Link>
+    <Link to='' className='nav'>About Us</Link>
+    <Link to='' className='nav'>Contact Us</Link>
+    <Link to='' className='nav'>Services</Link>
       
     </div>
   )
