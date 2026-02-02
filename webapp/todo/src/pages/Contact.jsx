fd06ee2e40718dd521675de9a808e5c9f12eb6e9
HarshRaj2910 <harshroman617@gmail.com>
2026-02-02 12:13:32 +0530

    g

--- webapp/todo/src/pages/Contact.jsx
+++ webapp/todo/src/pages/Contact.jsx
@@ -5,8 +5,10 @@ import { Outlet,Link } from 'react-router-dom'
 function Contact() {
   return (
     <div style={{textAlign:"center",marginTop: "18px",padding:"12px"}}>
-      <Link to='profile'>Profile</Link>
-      <Link to='accounts'>Accounts</Link>
+      <Link to='profile'  style={{padding: "14px",background:"coral",color: "#fff",
+      textDecoration: "none"}}>Profile</Link>
+      <Link to='accounts' style={{padding: "14px",background:"coral",color: "#fff",
+      textDecoration: "none",marginLeft:"12px",border:"none",borderRadius:"5px"}}>Accounts</Link>
       <Outlet/>
     </div>
   )
