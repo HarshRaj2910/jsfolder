683204f8ae0f131a8e193ad11e946377570cd678
HarshRaj2910 <harshroman617@gmail.com>
2026-02-03 12:44:26 +0530

    node

--- expserver.js
+++ expserver.js
@@ -2,4 +2,6 @@ const express=require("express")
 const app=express();
 
 app.get('/',(req,res)=> res.send('Hello world!'))
+app.get('/about',(req,res)=> res.send('About Page'))
+app.get('/contact',(req,res)=> res.send('Contact Page'))
 app.listen(4000,()=>console.log('Server started'));
