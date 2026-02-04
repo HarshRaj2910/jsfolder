56b3d7302d155e1b54ca2394d0f6a0fcb5ad07f8
HarshRaj2910 <harshroman617@gmail.com>
2026-02-04 10:44:25 +0530

    b

--- webapp/todo/src/pages/Contact.jsx
+++ webapp/todo/src/pages/Contact.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { use } from 'react'
 import { Outlet,Link } from 'react-router-dom'
+import { useState } from 'react'
 
 
 function Contact() {
+  const [value,setvalue]=useState('');
+  const handleChange=(e)=>{
+      setvalue(e.target.value);
+  }
+  const handleSubmit=(e)=>{
+
+  }
   return (
     <div style={{textAlign:"center",marginTop: "18px",padding:"12px"}}>
       <Link to='profile'  style={{padding: "14px",background:"coral",color: "#fff",
@@ -10,6 +18,17 @@ function Contact() {
       <Link to='accounts' style={{padding: "14px",background:"coral",color: "#fff",
       textDecoration: "none",marginLeft:"12px",border:"none",borderRadius:"5px"}}>Accounts</Link>
       <Outlet/>
+      <hr/>
+      <form onSubmit={handleSubmit}>
+        <input type='text' 
+        name='name'
+        value={value}
+        onChange={handleChange}
+        placeholder='Enter Name'/>
+        <button type='submit'>Click</button>
+      </form>
+      <h3>{value}</h3>
+
     </div>
   )
 }
