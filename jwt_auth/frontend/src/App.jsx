b8e49d1d7594f4d64856a3edd2d1008456ee0172
HarshRaj2910 <harshroman617@gmail.com>
2026-02-07 01:03:04 +0530

    a

--- jwt_auth/frontend/src/App.jsx
+++ jwt_auth/frontend/src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import Signup from './Signup'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import Login from './Login'
 
 function App() {
   return (
     <div>
       
       <BrowserRouter>
-      <Routes>
-      <Route path='/Signup' element={<Signup/>}></Route>
-      </Routes>
-
+        <Routes>
+              <Route path='/' element={<Signup/>}></Route> 
+              <Route path='/signup' element={<Signup/>}></Route> 
+              <Route path='/login' element={<Login/>}></Route>
+       </Routes>
       </BrowserRouter>
+      <ToastContainer/>
     </div>
   )
 }
--- jwt_auth/frontend/src/Signup.jsx
+++ jwt_auth/frontend/src/Signup.jsx
@@ -1,17 +1,66 @@
-
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { handleError, handleSuccess } from './utils';
+import  './Signup.css'
+// import { signup } from '../../Backend/Controllers/AuthController';
 
 function Signup() {
+    const [signupInfo, setSignupInfo]=useState({
+        name:'',
+        email:'',
+        password:''
+    });
+    const navigate=useNavigate();
+    const handleChange=(e)=>{
+        const {name, value}=e.target;
+        const copySignupInfo={ ...signupInfo};
+        copySignupInfo[name] = value;
+        setSignupInfo(copySignupInfo);
+    }
+
+    const handleSignup=async (e)=>{
+        e.preventDefault();
+        const { name,email,password }= signupInfo;
+
+        try{
+            const url=`http://localhost:8000/auth/signup`;
+            const response=await fetch(url, {
+                method:"POST",
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify(signupInfo)
+            });
+            const result=await response.json();
+            const { success, message, error }=result;
+            if(success){ 
+                handleSuccess(message);
+                setTimeout(()=>{
+                    navigate('/login')
+                }, 2000)
+            }else if(error){
+                const details=error?.details[0].message;
+                handleError(details);
+            }else if(!success){
+                handleError(message);
+            }
+        }catch(err){
+            handleError(err);
+        }
+    }
     return(
         <div className='form-container'> 
-    <h2>Registration Form</h2>
-    <form >
-        <div className='form-group'>
-            <label htmlFor='Name'>Name:</label>
+            <h2>Registration Form</h2>
+            <form  onSubmit={handleSignup}>
+             <div className='form-group'>
+            <label htmlFor='name'>Name:</label>
             <input 
             type="text"
             name="name"
             id="name"
-            
+            onChange={handleChange}
+            value={signupInfo.name}
             placeholder="Enter your name"
             required
             /> 
@@ -21,8 +70,9 @@ function Signup() {
             <input 
             type="email"
             name="email"
-            
             id="email"
+            onChange={handleChange}
+            value={signupInfo.email}
            
             placeholder="Enter your email"
             required
@@ -33,8 +83,9 @@ function Signup() {
             <input 
             type="password"
             name="password"
-            
             id="password"
+            onChange={handleChange}
+            value={signupInfo.password}
             
             placeholder="Enter your password"
             required
@@ -49,3 +100,5 @@ function Signup() {
     </div>
   )
 }
+
+export default Signup
